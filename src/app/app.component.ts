46a4aad328fcd19e90e5b1fc5afad7647f231003
Johan Esneyder Rojas Latorre <johanesneyder10@gmail.com>
2018-01-16 17:20:38 -0500

    Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -8,20 +10,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  my_notes= [
-    {id: 1, title: 'Note 1', description: 'description for note 1'},
-    {id: 2, title: 'Note 2', description: 'description for note 1'},
-    {id: 3, title: 'Note 3', description: 'description for note 1'},
-    {id: 4, title: 'Note 4', description: 'description for note 1'}
-  ];
-  note={title:null, body:null};
+  my_notes: Observable<any[]>;
 
-  showform:boolean = false;
+  constructor(public afDB: AngularFireDatabase) {
+    this.getNotes().valueChanges().subscribe(notas => {
+      this.my_notes = notas;
+
+    });
+  }
+  getNotes(){
+    return this.afDB.list('/notas');
+  }
+  removeNotes(){
+    this.afDB.database.ref('notas/' + this.note.id).remove();
+    this.showform = false;
+    this.note = {id: null, title: null, description: null};
+  }
+  note = {id: null, title: null, description: null};
+  showform = false;
+  editing = false;
+  
   addNote() {
+    this.editing = true;
     this.showform = true;
+    this.note = {id: null, title: null, description: null};
   }
   cancel() {
     this.showform = false;
   }
-  
+  createNote(){
+    if(this.editing){     
+    this.afDB.database.ref('notas/' + this.note.id).set(this.note);
+    }else{
+    this.note.id = Date.now();
+    this.afDB.database.ref('notas/' + this.note.id).set(this.note);
+    }
+    this.showform = false;
+    this.note = {id: null, title: null, description: null};
+     /*if(this.editing){
+      var me = this;
+      this.my_notes.forEach((el, i) => {
+        if(el.id == me.note.id){
+          me.my_notes[i] = me.note;
+        }
+      });
+      this.showform = false;
+    }else{    
+    this.my_notes.push(this.note);
+    this.showform = false;
+    this.note = {id: null, title: null, description: null};
+    }*/
+  }
+  viewNote(note) {
+    this.editing = true;
+    this.note = note; 
+    this.showform = true;
+  }
+  delete(){
+    /*var me = this;
+    this.my_notes.forEach((el, i) => {
+      if(el == me.note){
+        me.my_notes.splice(i, 1);
+      }
+    });
+    this.showform = false;
+    this.note = {id: null, title: null, description: null};*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDHCWVAJs3WTJyaNvVyjqf-Hqf7ZzoQ_4w",
+  authDomain: "ng-notas-1c434.firebaseapp.com",
+  databaseURL: "https://ng-notas-1c434.firebaseio.com",
+  storageBucket: "ng-notas-1c434.appspot.com",
+  messagingSenderId: '987013240486'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
