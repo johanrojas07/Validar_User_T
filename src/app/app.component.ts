012d2f36f967af104d3cd5527e03aec98ee75d61
Johan <johanesneyder10@gmail.com>
2018-01-15 21:51:40 -0500

    Casa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  my_notes= [
+    {id: 1, title: 'Note 1', description: 'description for note 1'},
+    {id: 2, title: 'Note 2', description: 'description for note 1'},
+    {id: 3, title: 'Note 3', description: 'description for note 1'},
+    {id: 4, title: 'Note 4', description: 'description for note 1'}
+  ];
+  note={title:null, body:null};
+
+  showform:boolean = false;
+  addNote() {
+    this.showform = true;
+  }
+  cancel() {
+    this.showform = false;
+  }
+  
 }
